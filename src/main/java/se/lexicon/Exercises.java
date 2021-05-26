2b1f823b0e5b7922507131451096432c32bfda9d
olof-sky <oloof92@gmail.com>
2021-05-26 14:12:29 +0200

    Exercise finished

---
+++ src/main/java/se/lexicon/Exercises.java
@@ -0,0 +1,146 @@
+package se.lexicon;
+
+import se.lexicon.data.DataStorage;
+import se.lexicon.model.Gender;
+import se.lexicon.model.Person;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Comparator;
+
+public class Exercises {
+
+    private final static DataStorage storage = DataStorage.INSTANCE;
+
+    /*
+       1.	Find everyone that has firstName: “Erik” using findMany().
+    */
+    public static void exercise1(String message){
+        System.out.println(message);
+        storage.findMany(person -> person.getFirstName().equals("Erik")).forEach(System.out::println);
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        2.	Find all females in the collection using findMany().
+     */
+    public static void exercise2(String message){
+        System.out.println(message);
+        storage.findMany(person -> person.getGender().equals(Gender.FEMALE)).forEach(System.out::println);
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        3.	Find all who are born after (and including) 2000-01-01 using findMany().
+     */
+    public static void exercise3(String message){
+        System.out.println(message);
+        storage.findMany(person -> person.getBirthDate().isAfter(LocalDate.parse("1999-12-31"))).forEach(System.out::println);
+        System.out.println("----------------------");
+    }
+
+    /*
+        4.	Find the Person that has an id of 123 using findOne().
+     */
+    public static void exercise4(String message){
+        System.out.println(message);
+        System.out.println(storage.findOne(person -> person.getId() == (123)));
+
+        System.out.println("----------------------");
+
+    }
+
+    /*
+        5.	Find the Person that has an id of 456 and convert to String with following content:
+            “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().
+     */
+    public static void exercise5(String message){
+        System.out.println(message);
+        System.out.println(storage.findOneAndMapToString(person -> person.getId() == (456), person -> "Name: " + person.getFirstName() + " " + person.getLastName() + " born " + person.getBirthDate()));
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        6.	Find all male people whose names start with “E” and convert each to a String using findManyAndMapEachToString().
+     */
+    public static void exercise6(String message){
+        System.out.println(message);
+        System.out.println(storage.findManyAndMapEachToString(person -> person.getFirstName().startsWith("E"), Person::toString));
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        7.	Find all people who are below age of 10 and convert them to a String like this:
+            “Olle Svensson 9 years”. Use findManyAndMapEachToString() method.
+     */
+    public static void exercise7(String message){
+        System.out.println(message);
+        System.out.println(storage.findManyAndMapEachToString(person -> Period.between(person.getBirthDate(), LocalDate.now()).getYears() < 10, person -> person.getFirstName() + " " + person.getLastName() + " " + Period.between(person.getBirthDate(), LocalDate.now()).getYears() + " years"));
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        8.	Using findAndDo() print out all people with firstName “Ulf”.
+     */
+    public static void exercise8(String message){
+        System.out.println(message);
+        storage.findAndDo(person -> person.getFirstName().equals("Ulf"), System.out::println);
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        9.	Using findAndDo() print out everyone who have their lastName contain their firstName.
+     */
+    public static void exercise9(String message){
+        System.out.println(message);
+        storage.findAndDo(person -> person.getLastName().contains((person.getFirstName())), System.out::println);
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        10.	Using findAndDo() print out the firstName and lastName of everyone whose firstName is a palindrome.
+     */
+    public static void exercise10(String message){
+        System.out.println(message);
+        storage.findAndDo(person -> person.getFirstName().equalsIgnoreCase(new StringBuilder().append(person.getFirstName()).reverse().toString().trim()), System.out::println);
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        11.	Using findAndSort() find everyone whose firstName starts with A sorted by birthdate.
+     */
+    public static void exercise11(String message){
+        System.out.println(message);
+        System.out.println(storage.findAndSort(person -> person.getFirstName().startsWith("A"), Comparator.comparing(Person::getBirthDate)));
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        12.	Using findAndSort() find everyone born before 1950 sorted reversed by latest to earliest.
+     */
+    public static void exercise12(String message){
+        System.out.println(message);
+        System.out.println(storage.findAndSort(person -> person.getBirthDate().isBefore(LocalDate.parse("1950-01-01")), Comparator.comparing(Person::getBirthDate).reversed()));
+
+        System.out.println("----------------------");
+    }
+
+    /*
+        13.	Using findAndSort() find everyone sorted in following order: lastName > firstName > birthDate.
+     */
+    public static void exercise13(String message){
+        System.out.println(message);
+        System.out.println(storage.findAndSort(Comparator.comparing(Person::getLastName).thenComparing(Person::getFirstName).thenComparing(Person::getBirthDate)));
+
+        System.out.println("----------------------");
+    }
+}
---
+++ src/main/java/se/lexicon/data/DataStorageImpl.java
@@ -0,0 +1,110 @@
+package se.lexicon.data;
+
+
+import se.lexicon.model.Person;
+import se.lexicon.util.PersonGenerator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+/**
+ * Create implementations for all methods. I have already provided an implementation for the first method *
+ */
+public class DataStorageImpl implements DataStorage {
+
+    private static final DataStorage INSTANCE;
+
+    static {
+        INSTANCE = new DataStorageImpl();
+    }
+
+    private final List<Person> personList;
+
+    private DataStorageImpl(){
+        personList = PersonGenerator.getInstance().generate(1000);
+    }
+
+    static DataStorage getInstance(){
+        return INSTANCE;
+    }
+
+
+    @Override
+    public List<Person> findMany(Predicate<Person> filter) {
+        List<Person> result = new ArrayList<>();
+        for(Person person : personList){
+            if(filter.test(person)){
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Person findOne(Predicate<Person> filter) {
+        for(Person person : personList){
+            if(filter.test(person)){
+                return(person);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String findOneAndMapToString(Predicate<Person> filter, Function<Person, String> personToString){
+        for(Person person : personList){
+            if(filter.test(person)){
+                return(personToString.apply(person));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> findManyAndMapEachToString(Predicate<Person> filter, Function<Person, String> personToString){
+        List<String> result = new ArrayList<>();
+        for(Person person : personList){
+            if(filter.test(person)){
+                result.add(personToString.apply(person));
+            }
+        }
+        return result;
+    }
+
+
+    @Override
+    public void findAndDo(Predicate<Person> filter, Consumer<Person> consumer){
+        for(Person person : personList) {
+            if (filter.test(person)) {
+                consumer.accept(person);
+            }
+        }
+    }
+
+    @Override
+    public List<Person> findAndSort(Predicate<Person> filter, Comparator<Person> comparator){
+        List<Person> result = new ArrayList<>();
+        for (Person person : personList){
+            if (filter.test(person)){
+                result.add(person);
+            }
+        }
+        result.sort(comparator);
+        return result;
+    }
+
+    @Override
+    public List<Person> findAndSort(Comparator<Person> comparator){
+        List<Person> result = new ArrayList<>();
+        for (Person person : personList){
+            result.add(person);
+        }
+        result.sort(comparator);
+        return result;
+    }
+}
